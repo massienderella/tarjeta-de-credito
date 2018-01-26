5e82315ce91010074b8baf8ac6a1beb736a92bdf
Massienderella <massiel.ams@gmail.com>
2018-01-26 12:52:36 -0300

    style guide applied to js file

--- isValidCard.js
+++ isValidCard.js
@@ -1,4 +1,4 @@
-function isValidCard(cardNumber){ //funcion
+/* function isValidCard(cardNumber){ //funcion
 	var cardNumber = prompt("Ingrese el número de tarjeta a verificar") //usuario ingresa su número de tarjeta mediante un prompt(), se almacena en var cardNumber
 	var arrCardNumber = Array.from(cardNumber); //números separados
 	var arrNumbersInEvenIndexes = [] //arr donde se almacenan los números en posiciones impares
@@ -14,4 +14,48 @@ function isValidCard(cardNumber){ //funcion
 		var evenIndexes = (arrNumbersInEvenIndexes *2) // multiplicar los números en posiciones pares *2???
 
 
+} */
+
+function validNumbers() {
+  var cardNumber = '';
+  do {
+    cardNumber = prompt('Ingrese los números de su tarjeta de crédito a validar');
+    if (cardNumber !== '') {
+      isValidCard(cardNumber);
+    } else {
+      alert('Ingrese el número de su tarjeta de crédito');
+    }  
+  } while (cardNumber === '');
 }
+
+function isValidCard(cardNumber) {
+  var cardNumberInv = [];
+  var result = 0;
+  for (var i = 0; i < cardNumber.length; i++) {
+    cardNumberInv.unshift(parseInt(cardNumber[i]));
+  }
+  cardNumberInv.unshift('N');
+
+  for (var j = 1; j < cardNumberInv.length; j++) {
+    var resultOdd = 0;
+    var resultPair10 = 0;
+    var resultPair = 0;
+    result = resultPair + resultOdd + resultPair10;
+    if (j % 2 !== 0) {
+      result += cardNumberInv[j];
+    } else if (j % 2 === 0) {
+      if (cardNumberInv[j] * 2 > 10) {
+        (cardNumberInv[j] - 10) + 1;
+        result += cardNumberInv[j];
+      } else {
+        result += (cardNumberInv[j] * 2);
+      }
+    }
+  }
+  if (result % 10 === 0) {
+    ('Felicidades, su tarjeta es válida');
+  } else {
+    document.write('Lo sentimos, su tarjeta no es válida');
+  }
+}
+validNumbers();
